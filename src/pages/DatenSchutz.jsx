968ce3bf3bd03281acf8f8d1bfb56ebf0429bcb1
Tairou Mouhamed <tairou97-mouhamed@hotmail.com>
2024-02-24 00:08:11 +0100

    change daten

--- src/pages/DatenSchutz.jsx
+++ src/pages/DatenSchutz.jsx
@@ -8,21 +8,19 @@ import { useContext } from "react";
 import { CursorContext } from "../context/CursorContext";
 
 const Datenschutz = () => {
-  const { mouserEnter, mouseLeaverEnter } = useContext(CursorContext);
-
   return (
     <motion.section
       initial={{ scale: 0, y: "100%" }}
       animate={{ scale: 1, y: 0 }}
       exit={{ scale: 0, y: "100%" }}
       transition={transition1}
-      className="section  "
+      className="section    "
       id="about"
     >
-      <div className="container mx-auto h-full  ">
+      <div className="container mx-auto h-full ">
         {/* text & img wrapper */}
         <div
-          className=" flex flex-col lg:flex-row  
+          className=" flex flex-col lg:flex-row  p-26
         h-full items-center justify-center gap-x-24 
         text-center lg:text-left lg:pt-16"
         >
@@ -32,9 +30,7 @@ const Datenschutz = () => {
             animate={{ scale: 1, y: 0 }}
             exit={{ scale: 0, y: "-80%" }}
             transition={transition1}
-            onMouseEnter={mouserEnter}
-            onMouseLeave={mouseLeaverEnter}
-            className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start  text-green-900 absolute top-0"
+            className="flex-1 pt-36 pb-14 p-10  lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start  text-green-900 absolute top-0"
           >
             <h1 className="h1">Datenschutzerklärung</h1>
             <p className="mb-5">
